6ca469ee72852d5ab549d1f4dcca2dc392a4da5e
verdi <verdi@DESKTOP-IE08CJ4>
2020-12-04 13:23:51 -0500

    Client can now request a poem xml file and save it

--- PoemIntel/src/http/PIClient.java
+++ PoemIntel/src/http/PIClient.java
@@ -1,10 +1,54 @@
 package http;
+import java.io.*;
+import java.net.*;
 
+/**
+ * This is a basic PIClient. It will connect to the localHost IP on port 8080
+ * every time. It will request a poem XML file from the server by name. 
+ * The file will be saved in this directory.
+ * 
+ * This work complies with the JMU Honor Code.
+ * 
+ * @author Robert Verdisco
+ * 
+ */
 public class PIClient {
 
 	public static void main(String[] args) {
-		
+        String poemName;
+        Socket s;
 
+        poemName = args[0];
+
+        try {
+            s = new Socket(InetAddress.getLocalHost(), 8080);
+
+            FileOutputStream fs = new FileOutputStream(poemName); 
+            OutputStream os = s.getOutputStream();
+            HttpOutputStream out = new HttpOutputStream(os);
+            HttpRequest req = new HttpRequest();
+            byte[] b;
+            
+
+            req.setMethod("GET");
+            URI uri = new URI(poemName);
+            req.setURI(uri);
+            req.write(out);
+
+            InputStream is = s.getInputStream();
+            b = is.readAllBytes();
+            s.close();
+            fs.write(b);
+            fs.close();
+
+            
+            
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
 	}
 
 }
