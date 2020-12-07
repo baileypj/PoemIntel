8ebbb6da89b8e2c58a2fd63cdca354fbb80f11df
Dylan Martin <Dylan Martin@DESKTOP-OM3GTSJ>
2020-12-07 15:20:59 -0500

    PoemHandler handles poems without newlines

--- PoemIntel/src/server/PoemHandler.java
+++ PoemIntel/src/server/PoemHandler.java
@@ -10,11 +10,14 @@ public class PoemHandler extends DefaultHandler
 {
   
   private boolean foundResponse;
-  private String title, author, year, body, root, waitingFor;
+  private String title, author, year, body, waitingFor;
   
-  public PoemHandler(String root)
+  public PoemHandler(String title)
   {
-    this.root = root;
+    this.title = title;
+    this.author = "";
+    this.year = "";
+    this.body = "";
   }
   
   public Poem getPoem()
@@ -33,16 +36,49 @@ public class PoemHandler extends DefaultHandler
   @Override
   public void startElement(String uri, String localName, String qName, Attributes attributes)
   {
+    if (foundResponse)
+    {
+      waitingFor = qName + "_characters";
+    }
+    else if (qName.equals("poem"))
+    {
+      waitingFor = "title_characters";
+    }
   }
 
   @Override
   public void endElement(String uri, String localName, String qName)
   {
+    if (qName.equals("poem"))
+      foundResponse = false;
+    if (foundResponse)
+      waitingFor = "";
   }
 
   @Override
   public void characters(char[] ch, int start, int length)
   {
+    if (waitingFor.equals("author_characters"))
+    {
+      author = new String(ch, start, length).trim();
+    } 
+    else if (waitingFor.equals("year_characters"))
+    {
+      year = new String(ch, start, length).trim();
+    }
+    else if (waitingFor.equals("body_characters"))
+    {
+      body += new String(ch, start, length).trim();
+    }
+    else if (waitingFor.equals("title_characters"))
+    {
+      String poemTitle = new String(ch, start, length).trim();
+      if (poemTitle.equals(this.title))
+      {
+        foundResponse = true;
+      }
+    }
+    
   }
 
 }
